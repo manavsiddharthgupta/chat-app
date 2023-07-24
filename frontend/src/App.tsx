54bfad738a2122d21e96044333177c542eaa28fd
Manav Gupta <2006468@kiit.ac.in>
2023-07-24 18:30:11 +0530

    added: useEffect if user exists

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
-
 function App() {
+	useEffect(() => {
+		fetch("http://localhost:4000/auth/success")
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-  return (
-    <div>
-        <h1>Hello World</h1>
-        <Link to="chat">chat</Link>
-      </div>
-  );
+	return (
+		<div>
+			<h1>Hello World</h1>
+			<Link to="chat">chat</Link>
+		</div>
+	);
 }
 
 export default App;
