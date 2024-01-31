030e07d0ac83a073c26e96f5ca7eb4889d7b6f14
Manav Gupta <2006468@kiit.ac.in>
2024-01-31 11:06:04 +0530

    update API endpoints

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,47 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import landingPage from "./assets/landingPage.png";
-import { MessagesSquare } from "lucide-react";
-import sendIcon from "./assets/sendIcon.png";
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import landingPage from './assets/landingPage.png'
+import { MessagesSquare } from 'lucide-react'
+import sendIcon from './assets/sendIcon.png'
 
 function App() {
-  const [isLoggedIn, setIfLogged] = useState(false);
+  const [isLoggedIn, setIfLogged] = useState(false)
 
   useEffect(() => {
-    const cookieString = document.cookie;
-    console.log(cookieString);
-    const cookies: any = {};
-    const cookieArray = cookieString.split(";");
+    const cookieString = document.cookie
+    console.log(cookieString)
+    const cookies: any = {}
+    const cookieArray = cookieString.split(';')
     cookieArray.forEach((cookie) => {
-      const [key, value] = cookie.trim().split("=");
-      cookies[key] = value;
-    });
-    const jwtToken = cookies.cookie;
+      const [key, value] = cookie.trim().split('=')
+      cookies[key] = value
+    })
+    const jwtToken = cookies.cookie
     if (jwtToken) {
-      setIfLogged(true);
+      setIfLogged(true)
     }
-  }, []);
+  }, [])
 
   const onHandleGoogleSignin = () => {
-    console.log("google signin");
-    window.open("http://localhost:4000/auth/google", "_self");
-  };
+    console.log('google signin')
+    window.open('https://sendo-server.onrender.com/auth/google', '_self')
+  }
   return (
-    <div className="bg-black min-h-screen text-white pt-20 pb-4 relative">
-      <div className="max-w-xl mx-auto text-center">
-        <h1 className="font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-pink-600 to-blue-400">
+    <div className='bg-black min-h-screen text-white pt-20 pb-4 relative'>
+      <div className='max-w-xl mx-auto text-center'>
+        <h1 className='font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-pink-600 to-blue-400'>
           Connect the World,
         </h1>
-        <h1 className="font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-blue-600 to-[#9bf9cf] inline-block mt-2">
+        <h1 className='font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-blue-600 to-[#9bf9cf] inline-block mt-2'>
           Chat in Realtime
         </h1>
-        <p className="font-semibold text-center text-[#ffffff88] mt-5">
+        <p className='font-semibold text-center text-[#ffffff88] mt-5'>
           Welcome to Sendo, Your Global Chat Hub with Room to Explore!
         </p>
-        <div className="mt-8">
+        <div className='mt-8'>
           {!isLoggedIn && (
             <button
-              className="rounded-3xl px-8 pt-[10px] pb-3 font-medium transition duration-300 bg-gradient-to-r from-[#f95d80] to-[#7180f5] hover:opacity-100 opacity-90"
+              className='rounded-3xl px-8 pt-[10px] pb-3 font-medium transition duration-300 bg-gradient-to-r from-[#f95d80] to-[#7180f5] hover:opacity-100 opacity-90'
               onClick={onHandleGoogleSignin}
             >
               Sign In With Google
@@ -49,23 +49,23 @@ function App() {
           )}
           {isLoggedIn && (
             <Link
-              className="rounded-3xl px-8 pt-[10px] pb-3 font-medium transition duration-300 bg-gradient-to-r from-[#f95d80] to-[#7180f5] hover:opacity-100 opacity-90"
-              to="chat"
+              className='rounded-3xl px-8 pt-[10px] pb-3 font-medium transition duration-300 bg-gradient-to-r from-[#f95d80] to-[#7180f5] hover:opacity-100 opacity-90'
+              to='chat'
             >
               chat
             </Link>
           )}
         </div>
         <img
-          className="absolute right-[18%] top-[40%]"
+          className='absolute right-[18%] top-[40%]'
           src={sendIcon}
-          alt="_send"
+          alt='_send'
         />
-        <MessagesSquare className="absolute left-[18%] top-[40%]" size={70} />
-        <img className="mt-12" src={landingPage} alt="_landingPage" />
+        <MessagesSquare className='absolute left-[18%] top-[40%]' size={70} />
+        <img className='mt-12' src={landingPage} alt='_landingPage' />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,98 +1,98 @@
-import { ChatBox } from "./ChatBox";
-import { useState, useEffect } from "react";
-import { Chatsidebar } from "./ChatSideBar";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import { split, HttpLink } from "@apollo/client";
-import { getMainDefinition } from "@apollo/client/utilities";
-import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
-import { createClient } from "graphql-ws";
-import { ChatTypes, myInfo } from "../lib/types";
-import jwt_decode from "jwt-decode";
-import { UserChatBox } from "./UserChatBox";
-import { useNavigate } from "react-router";
-import { ChakraProvider } from "@chakra-ui/react";
-import { InvalidChat } from "./InvalidChat";
+import { ChatBox } from './ChatBox'
+import { useState, useEffect } from 'react'
+import { Chatsidebar } from './ChatSideBar'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { split, HttpLink } from '@apollo/client'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
+import { ChatTypes, myInfo } from '../lib/types'
+import jwt_decode from 'jwt-decode'
+import { UserChatBox } from './UserChatBox'
+import { useNavigate } from 'react-router'
+import { ChakraProvider } from '@chakra-ui/react'
+import { InvalidChat } from './InvalidChat'
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/graphql",
-});
+  uri: 'https://sendo-server.onrender.com/graphql'
+})
 
 const wsLink = new GraphQLWsLink(
   createClient({
-    url: "ws://localhost:4000/graphql/subscription",
+    url: 'ws://sendo-server.onrender.com/graphql/subscription'
   })
-);
+)
 
 const splitLink = split(
   ({ query }) => {
-    const definition = getMainDefinition(query);
+    const definition = getMainDefinition(query)
     return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
-    );
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
   },
   wsLink,
   httpLink
-);
+)
 
 const client = new ApolloClient({
   link: splitLink,
-  cache: new InMemoryCache(),
-});
+  cache: new InMemoryCache()
+})
 // chat -> 'akjhsxkahskajk' to { type: 'room', id: 'akjhsxkahskajk' } | { type: 'user', id: 'akjhsxkahskajk'}
 export const Chat = () => {
-  const [chat, setChat] = useState<ChatTypes>({ type: "none", id: "" });
+  const [chat, setChat] = useState<ChatTypes>({ type: 'none', id: '' })
   const [myInfo, setMyInfo] = useState<myInfo>({
-    id: "",
-    name: "Anonymous",
-    email: "",
+    id: '',
+    name: 'Anonymous',
+    email: '',
     avatar:
-      "https://fastly.picsum.photos/id/379/536/354.jpg?hmac=I4bs_0ZcfxuA6apwsLHEPAqDxBprHAwMwtdoK8oJCOU",
+      'https://fastly.picsum.photos/id/379/536/354.jpg?hmac=I4bs_0ZcfxuA6apwsLHEPAqDxBprHAwMwtdoK8oJCOU',
     exp: 0,
-    iat: 0,
-  });
+    iat: 0
+  })
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   useEffect(() => {
-    const cookieString = document.cookie;
-    console.log(cookieString);
-    const cookies: any = {};
-    const cookieArray = cookieString.split(";");
+    const cookieString = document.cookie
+    console.log(cookieString)
+    const cookies: any = {}
+    const cookieArray = cookieString.split(';')
     cookieArray.forEach((cookie) => {
-      const [key, value] = cookie.trim().split("=");
-      cookies[key] = value;
-    });
-    const jwtToken = cookies.cookie;
+      const [key, value] = cookie.trim().split('=')
+      cookies[key] = value
+    })
+    const jwtToken = cookies.cookie
     if (jwtToken) {
-      const decoded: myInfo = jwt_decode(jwtToken);
-      setMyInfo(decoded);
+      const decoded: myInfo = jwt_decode(jwtToken)
+      setMyInfo(decoded)
     } else {
-      navigate("/");
+      navigate('/')
     }
-  }, [navigate]);
+  }, [navigate])
 
   const onSelectRoom = (roomId: string) => {
-    setChat({ type: "room", id: roomId });
-  };
+    setChat({ type: 'room', id: roomId })
+  }
 
   const onSelectUser = (userId: string) => {
-    setChat({ type: "user", id: userId });
-  };
+    setChat({ type: 'user', id: userId })
+  }
 
   const chatComp =
-    chat.type === "none" ? (
+    chat.type === 'none' ? (
       <InvalidChat />
-    ) : chat.type === "room" ? (
+    ) : chat.type === 'room' ? (
       <ChatBox myId={myInfo.id} email={myInfo.email} roomId={chat.id} />
     ) : (
       <UserChatBox myId={myInfo.id} email={myInfo.email} userId={chat.id} />
-    );
+    )
 
   return (
     <ApolloProvider client={client}>
       <ChakraProvider>
-        <div className="flex bg-black">
+        <div className='flex bg-black'>
           {chatComp}
           <Chatsidebar
             myProfile={myInfo}
@@ -102,5 +102,5 @@ export const Chat = () => {
         </div>
       </ChakraProvider>
     </ApolloProvider>
-  );
-};
+  )
+}
